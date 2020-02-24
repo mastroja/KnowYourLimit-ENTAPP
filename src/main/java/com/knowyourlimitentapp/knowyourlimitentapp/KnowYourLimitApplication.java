d79bc95a2b53c7e724985a65b423cdad4aa365be
jacob <54485488+mastroja@users.noreply.github.com>
2020-02-23 19:39:26 -0500

    3rd commit

--- src/main/java/com/knowyourlimitentapp/knowyourlimitentapp/KnowYourLimitApplication.java
+++ src/main/java/com/knowyourlimitentapp/knowyourlimitentapp/KnowYourLimitApplication.java
@@ -3,7 +3,7 @@ package com.knowyourlimitentapp.knowyourlimitentapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages= {"com.knowyourlimitentapp", "com.knowyourlimit.dto", "com.knowyourlimitentapp.knowyourlimitentapp", "com.knowyourlimit.service"})
 public class KnowYourLimitApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/knowyourlimitentapp/knowyourlimitentapp/KnowYourLimitController.java
+++ src/main/java/com/knowyourlimitentapp/knowyourlimitentapp/KnowYourLimitController.java
@@ -2,6 +2,7 @@ package com.knowyourlimitentapp.knowyourlimitentapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,8 +17,9 @@ public class KnowYourLimitController {
 	private iDrinkService drinkServiceStub;
 	
 	@RequestMapping(value="/start", method=RequestMethod.GET)
-	public String read() {
+	public String read(Model model) {
 		DrinkDTO drinkDTO = drinkServiceStub.fetchById(1);
+		model.addAttribute("drinkDTO", drinkDTO);
 		return "start";
 	}
 	
